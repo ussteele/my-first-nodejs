e544217406bcecfc574844c53e06e7ed65f95967
ussteele <ussteele@users.noreply.github.com>
2015-04-01 18:15:56 -0700

    Create routes.js

---
+++ routes.js
@@ -0,0 +1,22 @@
+module.exports = [
+	{
+		url: '/index',
+		controller: 'PagesController@index'
+	},  
+	{
+		url: '/about',
+		controller: 'PagesController@about'
+	},
+  {
+		url: '/dbase',
+		controller: 'PagesController@database'
+	},
+  {
+		url: '/weather',
+		controller: 'PagesController@weather'
+	},    
+  {
+		url: '/contact',
+		controller: 'PagesController@contact'
+	}
+];
